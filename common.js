c3748b668ddd0b58ef2a04892bb056d6c364fae2
garettridge <garett.ridge.github@gmail.com>
2019-02-07 23:41:58 -0800

    Created controls_widget
    Moved controls into their own class.  Both displaying scenes and rendering controls is now done by tree traversal through each scene's children, recursively.  Still need to respond to requests to move / hide panels, and still need to put a cap on string refresh rate.

--- common.js
+++ common.js
@@ -355,22 +355,6 @@ class Basic_Shader extends Shader             // Subclasses of Shader each store
 }
 
 
-window.Minimal_Webgl_Demo = window.classes.Minimal_Webgl_Demo =
-class Minimal_Webgl_Demo extends Scene_Component
-{ constructor( context )
-    { super( context );
-      this.submit_shapes( context, { triangle : new Minimal_Shape() } );         // Send a Triangle's vertices to the GPU's buffers.
-      this.shader = context.get_instance( Basic_Shader ).material();
-    }
-  display( graphics_state )                                                      // Do this every frame.
-    { this.shapes.triangle.draw( graphics_state, Mat4.identity(), this.shader ); // Draw the triangle.    
-    }
- make_control_panel()                 // Draw buttons, setup their actions and keyboard shortcuts, and monitor live variables.
-    { this.control_panel.innerHTML += "(This one has no controls)";
-    }
-}
-
-
 window.Funny_Shader = window.classes.Funny_Shader =
 class Funny_Shader extends Shader         // Simple "procedural" texture shader, with texture coordinates but without an input image.
 { material() { return { shader: this } }  // Materials here are minimal, without any settings.
