054054d0278ef0423af6d53a34ab4d4163768afd
garettridge <garett.ridge.github@gmail.com>
2019-04-24 23:10:43 -0700

    Basic_Phong_Complete
    Preparation for multiple lights
    
    Reduced varyings count (previously many for each light) due to H,E,L computed in fragment shader now (more accurate this way, especially for H according to Alan, but slower)

--- minimal-phong.js
+++ minimal-phong.js
@@ -364,6 +364,121 @@ export class Basic_Phong_Optimized extends Shader
     }
 }
 
+
+export class Basic_Phong_Complete extends Shader
+{                                  // **Phong_Shader** is a subclass of Shader, which stores and maanges a GPU program.  
+                                   // Graphic cards prior to year 2000 had shaders like this one hard-coded into them
+                                   // instead of customizable shaders.  "Phong" Shading is a process of determining 
+                                   // brightness of pixels via vector math.  It compares the normal vector at that 
+                                   // pixel to the vectors toward the camera and light sources.
+
+  
+  constructor( num_lights )
+    { super(); 
+      this.num_lights = num_lights;
+    }
+
+  shared_glsl_code()           // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
+    { return ` precision mediump float;
+        const int N_LIGHTS = ` + this.num_lights + `;
+        uniform float ambient, diffusivity, specularity, smoothness;
+        uniform vec4 light_position_or_vector[N_LIGHTS], light_color[N_LIGHTS];
+        uniform vec4 shape_color;
+        uniform vec3 squared_scale, camera_center;
+
+                              // Specifier "varying" means a variable's final value will be passed from the vertex shader
+                              // on to the next phase (fragment shader), then interpolated per-fragment, weighted by the
+                              // pixel fragment's proximity to each of the 3 vertices (barycentric interpolation).
+        varying vec3 N, vertex_worldspace;
+                                                                                    
+        vec3 phong_model_light( vec3 N )
+          { 
+            vec3 E = normalize( camera_center - vertex_worldspace );
+
+                                                              // Light positions use homogeneous coords.  Use w = 0 for a
+                                                              // directional light source -- a vector instead of a point:
+            vec3 L = normalize( light_position_or_vector[0].xyz - light_position_or_vector[0].w * vertex_worldspace );
+            vec3 H = normalize( L + E );
+
+            float diffuse  =      max( dot( N, normalize( L ) ), 0.0 );
+            float specular = pow( max( dot( N, normalize( H ) ), 0.0 ), smoothness );
+
+            return shape_color.xyz * diffusivity * diffuse + light_color[0].xyz * specularity * specular;
+          } ` ;
+    }
+  vertex_glsl_code()           // ********* VERTEX SHADER *********
+    { return `
+        attribute vec3 position, normal;                            // Position is expressed in object coordinates.
+        
+        uniform mat4 model_transform;
+        uniform mat4 projection_camera_model_transform;
+
+        void main()
+          {                                                                   // The vertex's final resting place (in NDCS):
+            gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
+                                                                              // The final normal vector in screen space.
+            N = normalize( mat3( model_transform ) * normal / squared_scale);
+            
+            vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
+          } ` ;
+    }
+  fragment_glsl_code()         // ********* FRAGMENT SHADER ********* 
+    {                          // A fragment is a pixel that's overlapped by the current triangle.
+                               // Fragments affect the final image or get discarded due to depth.                                 
+      return `
+        void main()
+          { 
+                                                                      // Compute an initial (ambient) color:
+            gl_FragColor = vec4( shape_color.xyz * ambient, shape_color.w );
+                                                                     // Compute the final color with contributions from lights:
+            gl_FragColor.xyz += phong_model_light( normalize( N ) );
+          } ` ;
+    }
+  update_GPU( context, gpu_addresses, g_state, model_transform, material )
+    {             // update_GPU(): Define how to synchronize our JavaScript's variables to the GPU's.  This is where the shader 
+                  // recieves ALL of its inputs.  Every value the GPU wants is divided into two categories:  Values that belong
+                  // to individual objects being drawn (which we call "Material") and values belonging to the whole scene or 
+                  // program (which we call the "Program_State").  Send both a material and a program state to the shaders 
+                  // within this function, one data field at a time, to fully initialize the shader for a draw.                  
+      const gpu = gpu_addresses, gl = context;
+
+      const defaults = { color: Color.of( 0,0,0,1 ), ambient: 0, diffusivity: 1, specularity: 1, smoothness: 40 };
+      material = Object.assign( {}, defaults, material );
+                                                      // Send the current matrices to the shader.  Go ahead and pre-compute
+                                                      // the products we'll need of the of the three special matrices and just
+                                                      // cache and send those.  They will be the same throughout this draw
+                                                      // call, and thus across each instance of the vertex shader.
+                                                      // Transpose them since the GPU expects matrices as column-major arrays.
+      const PCM = g_state.projection_transform.times( g_state.camera_inverse ).times( model_transform );
+      gl.uniformMatrix4fv( gpu.                  model_transform, false, Mat.flatten_2D_to_1D( model_transform.transposed() ) );
+      gl.uniformMatrix4fv( gpu.projection_camera_model_transform, false, Mat.flatten_2D_to_1D(             PCM.transposed() ) );
+
+                                         // Use the squared scale trick from "Eric's blog" instead of inverse transpose matrix:
+      const squared_scale = model_transform.reduce( 
+                                         (acc,r) => { return acc.plus( Vec.from(r).mult_pairs(r) ) }, Vec.of( 0,0,0,0 ) ).to3();                                            
+      gl.uniform3fv( gpu.squared_scale, squared_scale );
+
+                                                              // Send the desired shape-wide material qualities to the graphics
+                                                              // card, where they will tweak the Phong lighting formula.
+      gl.uniform4fv( gpu.shape_color,    material.color       );
+      gl.uniform1f ( gpu.ambient,        material.ambient     );
+      gl.uniform1f ( gpu.diffusivity,    material.diffusivity );
+      gl.uniform1f ( gpu.specularity,    material.specularity );
+      gl.uniform1f ( gpu.smoothness,     material.smoothness  );
+
+      const O = Vec.of( 0,0,0,1 ), camera_center = g_state.camera_transform.times( O ).to3();
+      gl.uniform3fv( gpu.camera_center, camera_center );
+                                             // Omitting lights will show only the material color, scaled by the ambient term:
+      if( !g_state.lights.length )
+        return;
+      gl.uniform4fv( gpu.light_color, g_state.lights[0].color );
+                                                                  // Light position uses homogeneous coords:
+      const light_position_or_vector = g_state.lights[0].position;
+      gl.uniform4fv( gpu.light_position_or_vector, light_position_or_vector );
+    }
+}
+
+
 export class Phong_Shader_1 extends Basic_Phong_Compute_H_E_L_Outside { }
 export class Phong_Shader_2 extends Basic_Phong_Compute_H_E_Outside   { }
 export class Phong_Shader_3 extends Basic_Phong_Optimized             { }
@@ -376,7 +491,8 @@ export class Phong_Comparison_Demo extends Scene
       this.shapes = { ball : new defs.Subdivision_Sphere(3) }
 
       this.index = 0;
-      this.shaders = [ new defs.Phong_Shader(), new Basic_Phong(), new Basic_Phong_Optimized(), 
+      this.shaders = [ new Basic_Phong_Complete(2), new defs.Phong_Shader(), new Basic_Phong(), 
+                       new Basic_Phong_Optimized(), 
                        new Basic_Phong_Compute_H_E_Outside(), new Basic_Phong_Compute_H_E_L_Outside() ];
 
       this.materials = this.shaders.map( s => new Material( s, { ambient:.2, smoothness:10, color: Color.of( 1,1,0,1 ) } ) );
