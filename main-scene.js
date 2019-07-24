e10ac34072f19fd5c106f17055de0ba51e65029b
garettridge <garett.ridge.github@gmail.com>
2019-07-23 21:30:04 -0700

    Update main-scene.js

--- main-scene.js
+++ main-scene.js
@@ -1,7 +1,7 @@
 import {tiny, defs} from './examples/common.js';
                                                   // Pull these names into this module's scope for convenience:
-const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, 
-         Light, Shape, Material, Shader, Texture, Scene } = tiny;
+const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
+        Canvas_Widget, Code_Widget, Text_Widget } = tiny;
 
     // Now we have loaded everything in the files tiny-graphics.js, tiny-graphics-widgets.js, and common.js.
     // This yielded "tiny", an object wrapping the stuff in the first two files, and "defs" for wrapping all the rest.
