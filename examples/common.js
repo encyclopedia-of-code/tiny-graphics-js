422a919877f424cb2c602c10d5cbf87814e51938
garettridge <garett.ridge.github@gmail.com>
2020-09-26 01:46:55 -0700

    unnecesarry include

--- examples/common.js
+++ examples/common.js
@@ -1,7 +1,7 @@
 import {tiny} from '../tiny-graphics.js';
                                                   // Pull these names into this module's scope for convenience:
 const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, 
-         Light, Shape, Material, Shader, Texture, Component } = tiny;
+         Shape, Material, Shader, Texture, Component } = tiny;
 
 const defs = {};
 
