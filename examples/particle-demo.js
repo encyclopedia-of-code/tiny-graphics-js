e9840a9c12508817058c6a92f8b2b94db50d32fa
garettridge <11892349+garettridge@users.noreply.github.com>
2020-12-12 19:33:58 -0800

    Particle squares always face the camera now, sized appropriately.  Added description.

--- examples/particle-demo.js
+++ examples/particle-demo.js
@@ -9,9 +9,11 @@ const Vortex = defs.Vortex =
           Object.assign (this, {center, size});
       }
       reset (initial_rotation, initial_angular_velocity) {
-          this.rotation         = this.drawn_location = initial_rotation;
-          this.angular_velocity = initial_angular_velocity;
-          this.previous         = {rotation: this.rotation.copy ()};
+          this.rotation             = initial_rotation;
+          this.angular_velocity     = initial_angular_velocity;
+          this.matrix_between_steps =
+            Mat4.translation (...this.center).times (initial_rotation).times (Mat4.scale (...this.size));
+          this.previous             = {rotation: this.rotation.copy ()};
           return this;
       }
       advance (time_amount) {
@@ -20,10 +22,11 @@ const Vortex = defs.Vortex =
             Mat4.rotation (time_amount * this.angular_velocity.norm (), ...this.angular_velocity.normalized ()));
       }
       blend_rotation (alpha) {
-          return this.rotation.map ((x, i) => vec4 (...this.previous.rotation[ i ]).mix (x, alpha));   // bad
+          return this.rotation.map ((x, i) => vec4 (...this.previous.rotation[ i ]).mix (x, alpha));   // bad idea
       }
       blend_state (alpha) {
-          this.drawn_location = this.blend_rotation (alpha).times (Mat4.scale (...this.size));
+          this.matrix_between_steps =
+            Mat4.translation (...this.center).this.blend_rotation (alpha).times (Mat4.scale (...this.size));
       }
       static intersect_sphere (p, margin = 0) {
           return p.dot (p) < 1 + margin;
@@ -32,29 +35,29 @@ const Vortex = defs.Vortex =
 
 const Particle = defs.Particle =
   class Particle {
-      constructor (center) {
-          this.center = center;
-      }
-      reset (initial_translation, linear_velocity) {
-          this.translation     = this.drawn_location = initial_translation;
+      reset (initial_position, linear_velocity) {
+          this.position        = this.position_between_steps = initial_position;
           this.linear_velocity = linear_velocity;
-          this.previous        = {translation: this.translation.copy ()};
-          this.temp_matrix     = Mat4.identity ();
+          this.previous        = {position: this.position.copy ()};
           return this;
       }
       advance (time_amount) {
-          this.previous = {translation: this.translation.copy ()};
-          this.translation.pre_multiply (
-            Mat4.translation (...this.linear_velocity.times (time_amount)));
+          this.previous = {position: this.position.copy ()};
+          this.position.add_by (...this.linear_velocity.times (time_amount));
       }
       blend_state (alpha) {
-          this.drawn_location = Mat4.translation (...this.previous.center.mix (this.center, alpha));
+          this.position_between_steps = this.previous.position.mix (this.position, alpha);
       }
   };
+
 
 export class Particle_Demo extends Simulation {
     init () {
         this.num_particles = 1000;
+
+        this.particles = Array( this.num_particles ).fill(0).map( () => new Particle() );
+        this.vortices = new Vortex( vec3( 0,0,0 ), vec3( 10,10,10 ) );
+
 
         this.shapes   = {particles: new Particle_Cloud (this.num_particles)};
         const shader  = new Particle_Shader ();
@@ -64,6 +67,10 @@ export class Particle_Demo extends Simulation {
         const s           = this.shapes.particles.arrays;
         const random_vecs = Array (this.num_particles).fill (0).map (x => vec3 (0, .01, 0).randomized (.05));
         s.offset          = s.offset.map ((x, i) => x.plus (random_vecs[ ~~(i / 4) ]));
+    }
+    render_explanation () {
+        this.document_region.innerHTML += `<p>Let's render fluids using multi-scale balls that rotate in place and store momentum for our particles.</p>
+                                        <p>Inspired by the "eigenfluids" method, which represents fluid flow as the combination of multi-scale basis functions that each sort of resemble a rotating flow.  We're not quite going to do Smooth Particle Hydrodynamics, which uses realistic fluid forces between particles but no background grid or background helpers like balls.</p>`;
     }
     render_animation (caller) {
         if ( !caller.controls) {
@@ -82,6 +89,7 @@ export class Particle_Demo extends Simulation {
         this.shapes.particles.copy_onto_graphics_card (caller.context, ["offset"], false);
     }
 }
+
 
 const Particle_Cloud = defs.Particle_Cloud =
   class Particle_Cloud extends Shape {
@@ -106,11 +114,14 @@ const Particle_Shader = defs.Particle_Shader =
 
         uniform mat4 model_transform;
         uniform mat4 projection_camera_model_transform;
+        uniform vec2 particle_square_size;
 
         void main() {
             vec3 temp = offset;
             temp[1] = mod( temp[1]+3.0,6.0)-3.0;
-            gl_Position = projection_camera_model_transform * vec4( position+temp, 1.0 );     // Move vertex to final space.
+            vec4 square_point = vec4( position, 1.0);
+            square_point.xy *= particle_square_size;
+            gl_Position = projection_camera_model_transform * vec4( temp, 1.0 ) + square_point;     // Move vertex to final space.\
                                               // The final normal vector in screen space.
             N = normalize( mat3( model_transform ) * normal / squared_scale);
 
@@ -124,12 +135,22 @@ const Particle_Shader = defs.Particle_Shader =
         varying vec2 f_tex_coord;
 
         void main() {
-            vec4 tex_color = vec4( .01/distance( f_tex_coord, vec2( .5,.5) ) - .2 );
+            vec4 tex_color = vec4( .01/distance( f_tex_coord, vec2( .5,.5) ) - .2);
             if( tex_color.w < .01 ) discard;
                                                                      // Compute an initial (ambient) color:
             gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w );
                                                                      // Compute the final color with contributions from lights:
             gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
+            
+        //    gl_FragColor.xyz = gl_FragCoord.xyz * vec3( 1./1080.5, 1./600.5, 0. );
           } `;
+      }
+      send_uniforms (gl, gpu, uniforms, model_transform) {
+          super.send_uniforms (gl, gpu, uniforms, model_transform);
+
+          const particle_square_size = [uniforms.projection_transform[ 0 ][ 0 ],
+                                        uniforms.projection_transform[ 1 ][ 1 ]];
+
+          gl.uniform2fv (gpu.particle_square_size, particle_square_size);
       }
   };
