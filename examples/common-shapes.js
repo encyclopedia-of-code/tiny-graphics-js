b738ac489300564f68456c0fea0b794a698c44ec
garettridge <11892349+garettridge@users.noreply.github.com>
2021-05-03 01:54:16 -0700

    minor reformatting

--- examples/common-shapes.js
+++ examples/common-shapes.js
@@ -10,18 +10,26 @@ const Triangle = defs.Triangle =
   class Triangle extends Shape {
       // **Triangle** The simplest possible 2D Shape – one triangle.  It stores 3 vertices,
       // each having their own 3D position, normal vector, and texture-space coordinate.
+
       constructor () {
-          // Name the values we'll define per each vertex:
-          super ("position", "normal", "texture_coord");
+          super ();
+
+          this.vertices[0] = { position: vec3 (0, 0, 0),
+                               normal: vec3 (0, 0, 1),
+                               texture_coord: Vector.of (0, 0) }
+
           // First, specify the vertex positions -- the three point locations of an imaginary triangle:
           this.arrays.position = [vec3 (0, 0, 0), vec3 (1, 0, 0), vec3 (0, 1, 0)];
+
           // Next, supply vectors that point away from the triangle face.  They should match up with
           // the points in the above list.  Normal vectors are needed so the graphics engine can
           // know if the shape is pointed at light or not, and color it accordingly.
           this.arrays.normal = [vec3 (0, 0, 1), vec3 (0, 0, 1), vec3 (0, 0, 1)];
+
           //  Lastly, each point also simultaneously exists somewhere in texture space (the
           //  X/Y pixel space belonging to any 2D images we might like to paint the shape with):
           this.arrays.texture_coord = [Vector.of (0, 0), Vector.of (1, 0), Vector.of (0, 1)];
+
           // Index into our vertices to connect them into a whole triangle:
           this.indices              = [0, 1, 2];
           // A position, normal, and texture coord fully describes one "vertex".  What's the "i"th vertex?  Simply
