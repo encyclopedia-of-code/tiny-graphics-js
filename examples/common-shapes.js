28e19def5c6e5a99ead199f70fe3b1fd69885fa1
Adrien Hadj-Chaib <adriennice06@yahoo.fr>
2021-05-16 23:03:56 -0700

    OBJ Model Loading

--- examples/common-shapes.js
+++ examples/common-shapes.js
@@ -547,3 +547,117 @@ const Minimal_Shape = defs.Minimal_Shape =
           this.fill_buffer( ["position"] );
       }
   };
+
+  const Shape_From_File = defs.Shape_From_File =
+  class Shape_From_File extends tiny.Shape
+  {                                   // **Shape_From_File** is a versatile standalone Shape that imports
+                                      // all its arrays' data from an .obj 3D model file.
+    constructor( filename )
+      { super( "position", "normal", "texture_coord" );
+      this.ready = false;
+                                      // Begin downloading the mesh. Once that completes, return
+                                      // control to our parse_into_mesh function.
+        this.load_file( filename );
+      }
+    load_file( filename )
+        {                             // Request the external file and wait for it to load.
+          return fetch( filename )
+            .then( response =>
+              { if ( response.ok )  return Promise.resolve( response.text() )
+                else                return Promise.reject ( response.status )
+              })
+            .then( obj_file_contents => this.parse_into_mesh( obj_file_contents ) )
+            .catch( error => { throw "OBJ file loader:  OBJ file either not found or is of unsupported format." } )
+        }
+    parse_into_mesh( data )
+      {                           // Adapted from the "webgl-obj-loader.js" library found online:
+        var verts = [], vertNormals = [], textures = [], unpacked = {};
+
+        unpacked.verts = [];        unpacked.norms = [];    unpacked.textures = [];
+        unpacked.hashindices = {};  unpacked.indices = [];  unpacked.index = 0;
+
+        var lines = data.split('\n');
+
+        var VERTEX_RE = /^v\s/;    var NORMAL_RE = /^vn\s/;    var TEXTURE_RE = /^vt\s/;
+        var FACE_RE = /^f\s/;      var WHITESPACE_RE = /\s+/;
+
+        for (var i = 0; i < lines.length; i++) {
+          var line = lines[i].trim();
+          var elements = line.split(WHITESPACE_RE);
+          elements.shift();
+
+          if      (VERTEX_RE.test(line))   verts.push.apply(verts, elements);
+          else if (NORMAL_RE.test(line))   vertNormals.push.apply(vertNormals, elements);
+          else if (TEXTURE_RE.test(line))  textures.push.apply(textures, elements);
+          else if (FACE_RE.test(line)) {
+            var quad = false;
+            for (var j = 0, eleLen = elements.length; j < eleLen; j++)
+            {
+                if(j === 3 && !quad) {  j = 2;  quad = true;  }
+                if(elements[j] in unpacked.hashindices)
+                    unpacked.indices.push(unpacked.hashindices[elements[j]]);
+                else
+                {
+                    var vertex = elements[ j ].split( '/' );
+
+                    unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                    unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
+                    unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+
+                    if (textures.length)
+                      {   unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 0]);
+                          unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 1]);  }
+
+                    unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 0]);
+                    unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 1]);
+                    unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 2]);
+
+                    unpacked.hashindices[elements[j]] = unpacked.index;
+                    unpacked.indices.push(unpacked.index);
+                    unpacked.index += 1;
+                }
+                if(j === 3 && quad)   unpacked.indices.push( unpacked.hashindices[elements[0]]);
+            }
+          }
+        }
+        {
+          const { verts, norms, textures } = unpacked;
+          var selection_of_attributes = [];
+
+          if (verts != [])
+          {
+            selection_of_attributes.push("position");
+            for( var j = 0; j < verts.length/3; j++ )
+              this.vertices[j].position = vec3( verts[ 3*j ], verts[ 3*j + 1 ], verts[ 3*j + 2 ] );
+          }
+
+          if (norms != [])
+          {
+            selection_of_attributes.push("normal");
+            for( var j = 0; j < verts.length/3; j++ )
+              this.vertices[j].normal = vec3( verts[ 3*j ], verts[ 3*j + 1 ], verts[ 3*j + 2 ] );
+          }
+
+          if (textures != [])
+          {
+            selection_of_attributes.push("texture_coord");
+            for( var j = 0; j < verts.length/3; j++ )
+              this.vertices[j].texture_coord = vec( textures[ 2*j ], textures[ 2*j + 1 ] );
+          }
+
+          this.indices = unpacked.indices;
+        }
+        this.normalize_positions( false );
+
+        //Deduce it from the obj data!
+        this.fill_buffer( selection_of_attributes );
+
+        this.ready = true;
+      }
+    draw( caller, uniforms, model_transform, material, type = "TRIANGLES", instances )
+      {               // draw(): Same as always for shapes, but cancel all
+                      // attempts to draw the shape before it loads:
+        if( this.ready )
+          super.draw( caller, uniforms, model_transform, material, type, instances );
+      }
+  };
